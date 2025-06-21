7b90d106a7fcc57b3d89aa14f8cb39c4e9321075
CamiloEEN <camiloceen@gmail.com>
2025-06-21 17:04:22 -0500

    Created models and succesfully comunicated with the database using sqlalchemy

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
 from .database import engine, Base
 
+#################### to test the db connection
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+
+from .database import SessionLocal
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+########################################################
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message":"backend is running"}
+
+#####DELETE THIS AFTER TEST############################
+@app.get("/dbtest")
+def test_db(db: Session = Depends(get_db)):
+    result = db.execute(text("SELECT nickname FROM users LIMIT 1")).fetchone()
+    return {"test_query": result[0] if result else "no users"}
+###############################################################
---
+++ backend/app/models.py
@@ -0,0 +1,64 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from .database import Base
+from datetime import datetime
+
+class User(Base):
+    __tablename__ = "users" # debe ser igual al nombre de la tabla en postgresql
+    id = Column(Integer, primary_key=True, index=True)
+    nickname = Column(String, unique=True, nullable = False)
+    username = Column(String, nullable = False)
+    usersurname = Column(String, nullable = False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+class Password(Base):
+    __tablename__ = "passwords"
+
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    password_hash = Column(String, nullable=False)
+
+class Image(Base):
+    __tablename__ = "images"
+
+    id = Column(Integer, primary_key=True)
+    filename = Column(String, nullable=False)
+    mime_type = Column(String, nullable=False)
+    size = Column(Integer)
+    uploaded_at = Column(DateTime, default=datetime.utcnow)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    image_id = Column(Integer, ForeignKey("images.id"), nullable=False)
+
+class Comment(Base):
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    content = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+class Like(Base):
+    __tablename__ = "likes"
+
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+class History(Base):
+    __tablename__ = "history"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    image_id = Column(Integer, ForeignKey("images.id"), nullable=False)
+    transformation_vector = Column(String)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+
+
